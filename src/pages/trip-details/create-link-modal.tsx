5db6244b6180761a113bf6a973fc0b72c75464c2
Lucas Abdouni <lucasabdouni@hotmail.com>
2024-07-19 11:32:17 -0300

    feat: implements get and create link

---
+++ src/pages/trip-details/create-link-modal.tsx
@@ -0,0 +1,73 @@
+import { Link2, Tag, X } from 'lucide-react'
+import { FormEvent } from 'react'
+import { useParams } from 'react-router-dom'
+import { Button } from '../../components/button'
+import { api } from '../../lib/axios'
+
+interface CreateLinkModalProps {
+  closeCreateLinkModal: () => void
+}
+
+export function CreateLinkModal({
+  closeCreateLinkModal,
+}: CreateLinkModalProps) {
+  const { tripId } = useParams()
+
+  async function createLink(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const data = new FormData(event.currentTarget)
+
+    const title = data.get('title')?.toString()
+    const url = data.get('url')?.toString()
+
+    await api.post(`/trips/${tripId}/links`, {
+      title,
+      url,
+    })
+
+    window.document.location.reload()
+  }
+
+  return (
+    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+      <div className="w-[640px] rounded-lg py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+        <div className="space-y-2">
+          <div className="flex items-center justify-between">
+            <h2 className="text-lg font-semibold">Cadastrar link</h2>
+            <button onClick={closeCreateLinkModal}>
+              <X className="size-5 text-zinc-400" />
+            </button>
+          </div>
+          <p className="text-sm text-zinc-400">
+            Todos convidados podem visializar os links importantes.
+          </p>
+        </div>
+
+        <form onSubmit={createLink} className="space-y-3">
+          <div className="h-14 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
+            <Tag className="text-zinc-400 size-5" />
+            <input
+              name="title"
+              placeholder="Título do link"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            />
+          </div>
+
+          <div className="h-14 flex-1 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
+            <Link2 className="text-zinc-400 size-5" />
+            <input
+              name="url"
+              placeholder="URL"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            />
+          </div>
+
+          <Button type="submit" variant="primary" size="full">
+            Salvar link
+          </Button>
+        </form>
+      </div>
+    </div>
+  )
+}
--- src/pages/trip-details/important-links.tsx
+++ src/pages/trip-details/important-links.tsx
@@ -1,40 +1,74 @@
 import { Link2, Plus } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import { Button } from '../../components/button'
+import { api } from '../../lib/axios'
+import { CreateLinkModal } from './create-link-modal'
+
+interface Links {
+  id: string
+  title: string
+  url: string
+}
 
 export function ImportantLinks() {
+  const { tripId } = useParams()
+  const [isCreateLinkModalOpen, setIsCreateLinkModalOpen] = useState(false)
+  const [links, setLinks] = useState<Links[]>([])
+
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/links`)
+      .then((response) => setLinks(response.data.links))
+  }, [tripId])
+
+  function openCreateLinkModal() {
+    setIsCreateLinkModalOpen(true)
+  }
+
+  function closeCreateLinkModal() {
+    setIsCreateLinkModalOpen(false)
+  }
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Links importantes</h2>
       <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">
-              Reserva do Airbnb
-            </span>
-            <a className="block text-zinc-400 truncate hover:text-zinc-200">
-              https://www.airbnb.com.br/rooms/104700011
-            </a>
-          </div>
-          <Link2 className="text-zinc-400 size-5 shrink-0" />
-        </div>
-
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">
-              Reserva do Airbnb
-            </span>
-            <a className="block text-zinc-400 truncate hover:text-zinc-200">
-              https://www.airbnb.com.br/rooms/104700011
-            </a>
-          </div>
-          <Link2 className="text-zinc-400 size-5 shrink-0" />
-        </div>
+        {links.map((link) => {
+          return (
+            <div
+              key={link.id}
+              className="flex items-center justify-between gap-4"
+            >
+              <div className="space-y-1.5">
+                <span className="block font-medium text-zinc-100">
+                  {link.title}
+                </span>
+                <a
+                  href={link.url}
+                  target="_blank"
+                  className="block text-zinc-400 truncate hover:text-zinc-200"
+                  rel="noreferrer"
+                >
+                  {link.url}
+                </a>
+              </div>
+              <a href={link.url} target="_blank" rel="noreferrer">
+                <Link2 className="text-zinc-400 size-5 shrink-0" />
+              </a>
+            </div>
+          )
+        })}
       </div>
 
-      <Button variant="secondary" size="full">
+      <Button onClick={openCreateLinkModal} variant="secondary" size="full">
         <Plus className="size-5" />
         Cadastrar novo link
       </Button>
+
+      {isCreateLinkModalOpen && (
+        <CreateLinkModal closeCreateLinkModal={closeCreateLinkModal} />
+      )}
     </div>
   )
 }
